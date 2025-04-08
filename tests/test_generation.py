f0ddb2dbd9ca57a8497998ab9c6dce13e3d6be61
alfonWaz13 <alfonwaz13@icloud.com>
2025-04-08 12:50:16 +0200

    feat(generation): update generation to be a class instead of a function
    - This way it is easier to work with the repository since it can be directly injected
    - Also it improves a lot the testability, since we can set our own generators mocking collaborators (repository)

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,9 +1,34 @@
-from src.generation import generate
+from unittest.mock import MagicMock
+
+from src.currency_trade_id_repository import AlreadySavedCurrencyTradeIdError
+from src.generation import CurrencyTradeIdGenerator
 
 
-def test_ids_are_unique():
-    ids = set()
-    for i in range(0, 10):
-        ids.add(generate())
+class TestGeneration:
 
-    assert len(ids) == 10
+    @classmethod
+    def setup_class(cls):
+        cls.repository = MagicMock()
+
+    @classmethod
+    def setup_method(cls):
+        cls.repository.reset_mock()
+
+    def test_generate_returns_a_currency_trade_id(self):
+        currency_trade_id_generator = CurrencyTradeIdGenerator(repository=self.repository)
+        currency_trade_id = currency_trade_id_generator.generate()
+        assert currency_trade_id is not None
+
+    def test_generate_stores_currency_trade_id(self):
+        currency_trade_id_generator = CurrencyTradeIdGenerator(repository=self.repository)
+        currency_trade_id_generator.generate()
+        assert self.repository.add_currency_trade_id.call_count == 1
+
+    def test_generate_calls_add_currency_trade_again_if_id_exists(self):
+        currency_trade_id_generator = CurrencyTradeIdGenerator(repository=self.repository)
+        self.repository.add_currency_trade_id.side_effect = [
+            AlreadySavedCurrencyTradeIdError("Duplicated ID"),
+            None
+        ]
+        currency_trade_id_generator.generate()
+        assert self.repository.add_currency_trade_id.call_count == 2
