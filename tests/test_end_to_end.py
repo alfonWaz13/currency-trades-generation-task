81107ba3a9db7ade419033100f58da34a3aeb3f6
alfonWaz13 <alfonwaz13@icloud.com>
2025-04-08 13:24:44 +0200

    test(test-end-to-end): add concurrent test from step 3/5

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from src.currency_trade_id_repository import MemoryCurrencyTradeIdRepository
 from src.generation import CurrencyTradeIdGenerator
 
@@ -28,3 +30,22 @@ class TestEndToEnd:
             generated_ids.update(self.currency_trade_id_generator.generate_bulk(1000))
             generated_count += 1
             assert len(generated_ids) == generated_count * 1000
+
+    def test_concurrent_bulk_generation(self):
+        generated_ids = set()
+        bulk_args = []
+        for _ in range(0, 2000):
+            bulk_args.append(2500)
+
+        def consumer_function(ids):
+            return list(ids)
+
+        with ThreadPoolExecutor(max_workers=9) as pool:
+            generators = list(pool.map(self.currency_trade_id_generator.generate_bulk, bulk_args))
+            ids = list(pool.map(consumer_function, generators))
+
+            for chunk in ids:
+                generated_ids.update(chunk)
+
+        assert len(generated_ids) == 5000000
+
