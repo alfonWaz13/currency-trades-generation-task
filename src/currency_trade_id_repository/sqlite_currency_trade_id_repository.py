225506aefccac6ae72c935160bdaa12d93d9b04e
alfonWaz13 <alfonwaz13@icloud.com>
2025-04-08 15:18:15 +0200

    feat(sqlite-currency-trade-id-repository): move connection creation inside class
    - There were problems related to the threads trying to use the same connection.
    - The use of a thread.Lock() has been implemented to avoid thread collision
    - Performance is very poor due to the bulk generation trying to open a connection every time generate is called

--- src/currency_trade_id_repository/sqlite_currency_trade_id_repository.py
+++ src/currency_trade_id_repository/sqlite_currency_trade_id_repository.py
@@ -1,4 +1,6 @@
 import sqlite3
+import threading
+from pathlib import Path
 from sqlite3 import IntegrityError
 
 from src.currency_trade_id import CurrencyTradeId
@@ -13,25 +15,28 @@ class SqliteCurrencyTradeIdRepository(CurrencyTradeIdRepository):
     _currency_trade_id_table = "currency_trade_ids"
     _id_column = "id"
 
-    def __init__(self, db_connection: sqlite3.Connection):
-        self.db_connection = db_connection
+    def __init__(self, db_path: Path):
+        self.db_path = db_path
+        self._threading_lock = threading.Lock()
 
         self._initialize_table()
 
     def _initialize_table(self) -> None:
-        cursor = self.db_connection.cursor()
-        cursor.execute(f"""
-            CREATE TABLE IF NOT EXISTS {self._currency_trade_id_table} (
-                {self._id_column} TEXT PRIMARY KEY
-            )
-        """)
-        self.db_connection.commit()
+        with sqlite3.connect(self.db_path) as connection:
+            cursor = connection.cursor()
+            cursor.execute(f"""
+                CREATE TABLE IF NOT EXISTS {self._currency_trade_id_table} (
+                    {self._id_column} TEXT PRIMARY KEY
+                )
+            """)
+            connection.commit()
 
     def add_currency_trade_id(self, currency_trade_id: CurrencyTradeId) -> None:
         try:
-            cursor = self.db_connection.cursor()
-            cursor.execute(f"INSERT INTO {self._currency_trade_id_table} ({self._id_column}) VALUES (?)",
-                           (str(currency_trade_id),))
-            self.db_connection.commit()
+            with self._threading_lock, sqlite3.connect(self.db_path) as connection:
+                cursor = connection.cursor()
+                cursor.execute(f"INSERT INTO {self._currency_trade_id_table} ({self._id_column}) VALUES (?)",
+                               (str(currency_trade_id),))
+                connection.commit()
         except IntegrityError:
             raise AlreadySavedCurrencyTradeIdError(currency_trade_id=currency_trade_id)
